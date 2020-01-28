7b8901c85fa4f93c7b787a575e35b1f23868bd52
OC-update <OC-update@bild.dtx.at>
2020-01-28 11:39:09 +0100

    changed setup.py for testing pip install

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rest_helper-OC-update@bild.dtx.at",
-    version="1.0",
+    version="1.02",
     author="OC-Updater",
     author_email="OC-update@bild.dtx.at",
     description="A python devops exercise",
@@ -13,13 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/OC-updater/rest_helper.git",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'configparser', 're', 'docopt', 'threading',
-    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3',
 )
